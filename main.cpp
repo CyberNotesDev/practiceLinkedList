8eeb1fcc1dcfa8e7ef817a6584dba0efb86e3c56
Elijah Littell <elittell214@gmail.com>
2021-02-06 12:41:14 -0500

    Made a simple Linked List set

--- main.cpp
+++ main.cpp
@@ -8,7 +8,26 @@ public:
 
 int main() {
 
-    
+    // Declaring values to be null and making them new Nodes
+
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* tail = NULL;
+
+    head = new Node();
+    second = new Node();
+    tail = new Node();
+
+    // Assigning the value and linking to the next data
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = tail;
+
+    tail->data = 3;
+    tail->next = NULL;
 
     return 0; 
 }
